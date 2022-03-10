069402589fe8de9197d256f4e1d1f318f0d1d540
Leivaa <56723478+leivaa21@users.noreply.github.com>
2022-03-10 16:20:29 +0100

    feat: ✨ added swagger module for autodocumentation (#1)

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import config from './app/app.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // CORS
   app.enableCors();
+  
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('UCOCamp Videos Microservice')
+    .setDescription('Users microservice API.')
+    .setVersion('1.0')
+    .addTag('videos')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+  
+  // App listen
   await app.listen(config.PORT);
 }
 bootstrap();
