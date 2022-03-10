71b549fa4c85c09fea45ff44c603191e63a29409
leivaa21 <adrianleiva212@gmail.com>
2022-03-10 22:17:27 +0100

    feat: ✨ Added multer module for uploading files

--- src/core/app/app.module.ts
+++ src/core/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import DatabaseModule from '../database/mongo/mongo.module';
+import MulterConfigModule from './multer.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MulterConfigModule],
   controllers: [],
   providers: [],
 })
