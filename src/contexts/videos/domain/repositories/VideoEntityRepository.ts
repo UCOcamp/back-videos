144220857b19c9aaffbd4b676aa2b2a38246e1df
leivaa21 <adrianleiva212@gmail.com>
2022-03-11 02:55:34 +0100

    fix: fixed returns types from repository interface, Only read methods should return entities

--- src/contexts/videos/domain/repositories/VideoEntityRepository.ts
+++ src/contexts/videos/domain/repositories/VideoEntityRepository.ts
@@ -3,9 +3,9 @@ import Video from '../entities/video';
 interface VideoEntityRepository {
   getOne(id: string): Promise<Video>;
   getAll(): Promise<Video[]>;
-  saveOne(video: Video): Promise<Video>;
-  updateOne(video: Video): Promise<Video>;
-  deleteOne(id: string): Promise<Video>;
+  saveOne(video: Video): Promise<void>;
+  updateOne(video: Video): Promise<void>;
+  deleteOne(id: string): Promise<void>;
 }
 
 export default VideoEntityRepository;
