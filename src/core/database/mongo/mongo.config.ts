222da931af09b611e98feb6d1ca86a0a9c08486c
leivaa21 <adrianleiva212@gmail.com>
2022-05-14 16:25:48 +0200

    feat: setting all up for using MongoDB Atlas Cluster

--- src/core/database/mongo/mongo.config.ts
+++ src/core/database/mongo/mongo.config.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 const config = {
   HOST: process.env.MONGO_HOST || 'localhost',
   NAME: process.env.MONGO_NAME || 'UCOCamp-videos',
+  USER: process.env.MONGO_USERNAME || '',
+  PASSWORD: process.env.MONGO_PASSWORD || '',
 };
 
 export default config;
