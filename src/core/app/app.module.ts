b3178ecd42e65ab4e8164de95431a588fe2c6c0a
Leivaa <56723478+leivaa21@users.noreply.github.com>
2022-05-15 12:19:36 +0200

    Feat: serving static files (#11)
    * feat: Serving static files folder in video/files

--- src/core/app/app.module.ts
+++ src/core/app/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import VideoModule from '../../contexts/videos/infrastructure/modules/Videos.module';
 import DatabaseModule from '../database/mongo/mongo.module';
 import MulterConfigModule from './multer.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [DatabaseModule, MulterConfigModule, VideoModule],
+  imports: [
+    DatabaseModule,
+    MulterConfigModule,
+    VideoModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), '/files'),
+      serveRoot: '/files',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
