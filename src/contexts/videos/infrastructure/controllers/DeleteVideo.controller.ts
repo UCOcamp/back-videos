4ab0448a7d8a53fa2be80cbf8d95af511e3932f9
leivaa21 <adrianleiva212@gmail.com>
2022-05-15 18:43:57 +0200

    doc: Added swagger docs for DeleteVideo endPoint

--- src/contexts/videos/infrastructure/controllers/DeleteVideo.controller.ts
+++ src/contexts/videos/infrastructure/controllers/DeleteVideo.controller.ts
@@ -6,16 +6,40 @@ import {
   Response,
 } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Response as res } from 'express';
 import DeleteVideoCommand from '../../application/useCases/DeleteVideo/commands/DeleteVideo.command';
 import DeleteVideoRequest from '../../application/useCases/DeleteVideo/requests/DeleteVideo.request';
 import DeleteVideoResponse from '../../application/useCases/DeleteVideo/responses/DeleteVideo.response';
 import deleteVideoOnStorage from './helpers/deleteVideoOnStorage';
 
+@ApiTags('video')
 @Controller('video')
 class DeleteVideoController {
   constructor(private readonly commandBus: CommandBus) {}
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete a video with his id',
+  })
+  @ApiParam({
+    description: 'Id of the video (UUID format)',
+    type: 'string',
+    name: 'id',
+  })
+  @ApiOkResponse({
+    status: 200,
+    description: 'Video removed succesfuly',
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Video Not Found!',
+  })
   async deleteVideo(@Param('id') id: string, @Response() response: res) {
     /**
      * Delete video on Database
